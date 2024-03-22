5b39ac1e10464d2239393f4a0dd59d079d459590
Mikhail Kazannik <kazannik.mikhail@yandex.ru>
2024-03-22 18:52:13 +0300

    feat: Sprint 10

---
+++ src/components/app/App.tsx
@@ -0,0 +1,53 @@
+import { CSSProperties, useState, FormEvent } from 'react';
+
+import { Article } from '../article/Article';
+import { ArticleParamsForm } from '../article-params-form/ArticleParamsForm';
+import { defaultArticleState } from '../../constants/articleProps';
+
+import '../../styles/index.scss';
+import styles from '../../styles/index.module.scss';
+
+export const App = () => {
+	const [pageState, setPageState] = useState(defaultArticleState);
+	const [formState, setFormState] = useState(defaultArticleState);
+
+	const resetForm = () => {
+		setFormState(defaultArticleState);
+		setPageState(defaultArticleState);
+	};
+
+	const submitForm = (e: FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+
+		setPageState({
+			...pageState,
+			fontFamilyOption: formState.fontFamilyOption,
+			fontSizeOption: formState.fontSizeOption,
+			fontColor: formState.fontColor,
+			contentWidth: formState.contentWidth,
+			backgroundColor: formState.backgroundColor,
+		});
+	};
+
+	return (
+		<main
+			className={styles.main}
+			style={
+				{
+					'--font-family': pageState.fontFamilyOption.value,
+					'--font-size': pageState.fontSizeOption.value,
+					'--font-color': pageState.fontColor.value,
+					'--container-width': pageState.contentWidth.value,
+					'--bg-color': pageState.backgroundColor.value,
+				} as CSSProperties
+			}>
+			<ArticleParamsForm
+				formState={formState}
+				setFormState={setFormState}
+				resetForm={resetForm}
+				submitForm={submitForm}
+			/>
+			<Article />
+		</main>
+	);
+};
--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -3,18 +3,158 @@ import { Button } from 'components/button';
 
 import styles from './ArticleParamsForm.module.scss';
 
-export const ArticleParamsForm = () => {
+import { Text } from '../text';
+import { RadioGroup } from '../radio-group';
+import { Separator } from '../separator';
+import { Select } from '../select';
+import {
+	fontFamilyOptions,
+	fontSizeOptions,
+	fontColors,
+	backgroundColors,
+	contentWidthArr,
+	OptionType,
+} from '../../constants/articleProps';
+
+import { FormEvent, useEffect, useRef, useState } from 'react';
+import clsx from 'clsx';
+
+type ArticleParamsForm = {
+	formState: {
+		fontFamilyOption: OptionType;
+		fontColor: OptionType;
+		backgroundColor: OptionType;
+		contentWidth: OptionType;
+		fontSizeOption: OptionType;
+	};
+	setFormState: React.Dispatch<
+		React.SetStateAction<{
+			fontFamilyOption: OptionType;
+			fontColor: OptionType;
+			backgroundColor: OptionType;
+			contentWidth: OptionType;
+			fontSizeOption: OptionType;
+		}>
+	>;
+	resetForm: () => void;
+	submitForm: (e: FormEvent<HTMLFormElement>) => void;
+};
+
+export function ArticleParamsForm(props: ArticleParamsForm) {
+	const [state, setState] = useState(false);
+
+	const toggleState = () => {
+		return setState(!state);
+	};
+
+	const rootRef = useRef<HTMLFormElement | null>(null);
+
+	useEffect(() => {
+		if (!state) return;
+
+		function handleOutsideClick(event: MouseEvent) {
+			const { target } = event;
+			const isOutsideClick =
+				target instanceof Node &&
+				rootRef.current &&
+				!rootRef.current.contains(target);
+
+			if (isOutsideClick) {
+				toggleState();
+			}
+		}
+
+		const handleEscape = (e: KeyboardEvent) => {
+			if (e.key === 'Escape') {
+				toggleState();
+			}
+		};
+
+		document.addEventListener('keydown', handleEscape);
+		document.addEventListener('mousedown', handleOutsideClick);
+
+		return () => {
+			document.removeEventListener('keydown', handleEscape);
+			document.removeEventListener('mousedown', handleOutsideClick);
+		};
+	}, [state]);
+
 	return (
 		<>
-			<ArrowButton />
-			<aside className={styles.container}>
-				<form className={styles.form}>
+			<ArrowButton state={state} toggleState={toggleState} />
+			<aside
+				className={clsx(styles.container, { [styles.container_open]: state })}>
+				<form className={styles.form} onSubmit={props.submitForm} ref={rootRef}>
+					<Text as='h2' size={31} weight={800} uppercase>
+						Задайте параметры
+					</Text>
+					<Select
+						selected={props.formState.fontFamilyOption}
+						onChange={(option) =>
+							props.setFormState({
+								...props.formState,
+								fontFamilyOption: option,
+							})
+						}
+						options={fontFamilyOptions}
+						placeholder='Open Sans'
+						title='шрифт'
+					/>
+					<RadioGroup
+						selected={props.formState.fontSizeOption}
+						onChange={(option) =>
+							props.setFormState({
+								...props.formState,
+								fontSizeOption: option,
+							})
+						}
+						options={fontSizeOptions}
+						name='font-size'
+						title='размер шрифта'
+					/>
+					<Select
+						selected={props.formState.fontColor}
+						onChange={(option) =>
+							props.setFormState({
+								...props.formState,
+								fontColor: option,
+							})
+						}
+						options={fontColors}
+						placeholder='Чёрный'
+						title='цвет шрифта'
+					/>
+					<Separator />
+					<Select
+						selected={props.formState.backgroundColor}
+						onChange={(option) =>
+							props.setFormState({
+								...props.formState,
+								backgroundColor: option,
+							})
+						}
+						options={backgroundColors}
+						placeholder='Белый'
+						title='цвет фона'
+					/>
+					<Select
+						selected={props.formState.contentWidth}
+						onChange={(option) =>
+							props.setFormState({
+								...props.formState,
+								contentWidth: option,
+							})
+						}
+						options={contentWidthArr}
+						placeholder='Широкий'
+						title='ширина контейнера'
+					/>
 					<div className={styles.bottomContainer}>
-						<Button title='Сбросить' type='reset' />
+						<Button title='Сбросить' type='reset' onClick={props.resetForm} />
 						<Button title='Применить' type='submit' />
 					</div>
 				</form>
 			</aside>
 		</>
 	);
-};
+}
